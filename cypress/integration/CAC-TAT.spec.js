37ec1cfe85c6098d20c1f8cd575060f0836312f5
jonathanrsbr <jonathanrsbr@gmail.com>
2023-11-08 00:22:03 -0300

    Adiciona requisição HTTP get e desestrutura o response, testando cada estrutura

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -185,13 +185,13 @@ const { should } = require("chai");
     });
     // lodash - time //
     // Vou refazer o teste da lesson02, dessa vez repetindo 5 vezes o envio, para verificar a estabilidade do teste //
-    Cypress._.times(5, () => {
-      it('envia o formuário com sucesso usando um comando customizado', () => {
+    Cypress._.times(5, function(){
+      it('envia o formuário com sucesso usando um comando customizado', function(){
       cy.fillMandatoryFieldsAndSubmit()
       });
     });
     // invoke('show') e invoke('hide')
-    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function(){
       cy.get('.success')
         .should('not.be.visible')
         .invoke('show')
@@ -207,6 +207,17 @@ const { should } = require("chai");
         .invoke('hide')
         .should('not.be.visible')
     });
+
+    // cy.request() permite fazer requisições http com cypress //
+    it('faz uma requisição HTTP', function(){
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response){
+          const { status, statusText, body} = response
+          expect(status).to.equal(200)
+          expect(statusText).to.equal("OK")
+          expect(body).to.include('CAC TAT')
+        })
+    })
   });
 
 
